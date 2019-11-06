1636d41f36b1cca89bf362bd34314bc30ba849aa
ubuntu <ubuntu@ip-172-31-95-30.ec2.internal>
2019-11-06 05:27:15 +0000

    assignment submission

--- src/main/java/com/stackroute/Exercise01StudentNameSort.java
+++ src/main/java/com/stackroute/Exercise01StudentNameSort.java
@@ -1,11 +1,14 @@
 package com.stackroute;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import javax.xml.bind.helpers.ParseConversionEventImpl;
 
 //Complete the code below as given in the problem.md file
 public class Exercise01StudentNameSort {
+	private static Scanner scan;
+
 	public static String[] sortStudents(String[] studentNames, int[] percentage) {
 		
 		for(int i=0;i<percentage.length-1;i++) {
@@ -27,22 +30,26 @@ public class Exercise01StudentNameSort {
 	//Do not print anything other than what's expected/asked in problem
 	public static void main(String[] args) {
 		//Use Scanner to get input from console
+	
 		
-		Scanner scan=new Scanner(System.in);
+		scan = new Scanner(System.in);
 		String inputData=scan.nextLine();
 		
-		String[] studentName = null;
-		int[] studentMarks = null;
-		
-		//System.out.println("inputData: "+inputData);
+		String[] studentName;
+		int[] studentMarks;
 		String[] studentinfo=inputData.split(";");
+		studentName=new String[studentinfo.length];
+		studentMarks=new int[studentinfo.length];
+		//System.out.println("inputData: "+inputData);
+		
 		for(int i=0;i<studentinfo.length;i++) {
+			
 			String[] studentDetail=studentinfo[i].split(":");
 			studentName[i]=studentDetail[0];
 			studentMarks[i]= Integer.parseInt(studentDetail[1]);
 		}
 		String[] studentNames=sortStudents(studentName,studentMarks);
 		
-		System.out.println(studentNames.toString());
+		System.out.println(Arrays.toString(studentName));
 	}
 }
